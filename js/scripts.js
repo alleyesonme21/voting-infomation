7ee283bca2d88914253869786acb9adc83bb460c
jd <duverseaujohnny21@gmail.com>
2020-06-04 14:29:17 -0400

    add scripts.js and form
    Co-authored-by: Nikolas Kososik <nikolas.kososik@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-$(function() {
-const vote_age = parseInt(prompt())
+$(document).ready(function() {
+  $("form#formVote").submit(function(event) {
+    event.preventDefault();
+
+    const age = $("#voting").val();
+    $("#output").show();
+
+    if (age >= 18) {
+      $("#vote-age").show();
+    } else {
+      $("#minor").show();
+    } 
+    
+  });
 });
